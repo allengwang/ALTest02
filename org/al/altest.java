4074df1958259fdb024bc863490970e07e102b48
王高见 <gaojianwang@Ctrip.com>
2016-01-05 18:12:57 +0800

    change altest

--- org/al/altest.java
+++ org/al/altest.java
@@ -6,7 +6,7 @@ public class altest {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello World");
-
+		System.out.println("Hello World");
 	}
 
 }
